e4d4f140c35c7c997219268796486b61a5063709
hinus <hinus@163.com>
2024-02-21 17:18:05 +0800

    Title: 实现CodeObject Description: 为加载CodeObject做好准备 Test Case: hello.pyc

---
+++ vm/code/codeObject.cpp
@@ -0,0 +1,26 @@
+#include "code/codeObject.hpp"
+
+CodeObject::CodeObject(int argcount, int nlocals, int posonlyargcount, int kwonlyargcount, int stacksize, 
+    int flag, HiString* bytecodes, ArrayList<HiObject*>* consts, 
+    ArrayList<HiObject*>* names, ArrayList<HiObject*>* varnames, 
+    ArrayList<HiObject*>* freevars, ArrayList<HiObject*>* cellvars,
+    HiString* file_name, HiString* co_name, int lineno, HiString* notable):
+        _argcount(argcount),
+        _posonlyargcount(posonlyargcount),
+        _kwonlyargcount(kwonlyargcount),
+        _nlocals(nlocals),
+        _stack_size(stacksize),
+        _flag(flag),
+        _bytecodes(bytecodes),
+        _names(names),
+        _consts(consts),
+        _var_names(varnames),
+        _free_vars(freevars),
+        _cell_vars(cellvars),
+        _co_name(co_name),
+        _file_name(file_name),
+        _lineno(lineno),
+        _notable(notable) {
+
+}
+
