90eb985a0847dc4089a1bab4e6a078fbba2bf899
feichao.ma <feichao.ma@shopee.com>
2024-11-09 19:56:08 +0800

    finish list study

--- test/test_list.py
+++ test/test_list.py
@@ -1,13 +1,23 @@
-l = [1, "hello"]
-l = ["hello", "world"]
+la = [1, "hello"]
+la = ["hello", "world"]
 
-if "hello" in l:
-    print "exist"
+if "hello" in la:
+    print("exist")
 else:
-    print "none"
+    print("none")
 
-print l[1]
+print(la[1])
 
 l = [4, 1, 2, 3]
+l.append(10)
+l[0] = 5
+del l[3]
+l.pop()
+l.remove(2)
 l.reverse()
-print l
+l.sort()
+print(l)
+print(l < la)
+
+for x in l:
+    print(x)
--- test/test_list_add.py
+++ test/test_list_add.py
@@ -1,12 +1,12 @@
 a = [1, 2]
 b = ["hello", "world"]
 c = a + b
-print a
-print b
-print c
+print(a)
+print(b)
+print(c)
 
 d = a * 3
-print d
+print(d)
 
 d.extend(b)
-print d
+print(d)
