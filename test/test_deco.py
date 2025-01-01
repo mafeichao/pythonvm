66fe6943f0c72c838688a5f263e5250fc396d64e
feichao.ma <feichao.ma@shopee.com>
2025-01-01 20:19:45 +0800

    finish decorator study

--- test/test_deco.py
+++ test/test_deco.py
@@ -1,8 +1,13 @@
+print(3)
+
 def call_cnt(fn):
+    count = 0
     cnt = [0, ]
     def inner_func(*args):
         cnt[0] += 1
-        print cnt[0]
+        nonlocal count
+        count += 1
+        print("count:" + str(cnt[0]) + "," + str(count))
         return fn(*args)
 
     return inner_func
@@ -11,5 +16,14 @@ def call_cnt(fn):
 def add(a, b = 2):
     return a + b
 
-print add(1, 2)
-print add(2, 3)
+print(add(1, 2))
+print(add(2, 3))
+
+@call_cnt
+def call_ex(a, b, c, d):
+    return a + b + c + d
+
+args = [3, 4]
+a = [1, ]
+b = (2, )
+print(call_ex(*a, *b, *args))
